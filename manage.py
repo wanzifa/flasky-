d9f74e1bacb21ed24fa614189480faa19f90b7da
王怡凡 <wangyifan@wangyifandeMacBook-Pro.local>
2015-10-01 20:09:13 +0800

    add

---
+++ manage.py
@@ -0,0 +1,36 @@
+import os
+from app import create_app, db
+from app.models import User, Follow, Role, Permission, Post, Comment
+from flask.ext.script import Manager, Shell
+from flask.ext.migrate import Migrate, MigrateCommand
+
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+manager = Manager(app)
+migrate = Migrate(app, db)
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
+                Permission=Permission, Post=Post, Comment=Comment)
+manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
+
+
+@manager.command
+def deploy():
+    """Run deployment tasks."""
+    from flask.ext.migrate import upgrade
+    from app.models import Role, User
+
+    # migrate database to latest revision
+    upgrade()
+
+    # create user roles
+    Role.insert_roles()
+
+    # create self-follows for all users
+    User.add_self_follows()
+
+
+if __name__ == '__main__':
+    manager.run()
